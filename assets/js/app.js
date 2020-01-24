64b601fbc8003d57b79c8091604a181f2d5860cd
Yensey Murillo <yenseymurillo@gmail.com>
2020-01-24 12:46:16 -0600

    added id to btn and firebase config

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+var config = {
+    apiKey: "AIzaSyBuib9w9qRVx0KRBTzV4u9CUk7AwtZ-aPk",
+    authDomain: "trainscheduler-edaa6.firebaseapp.com",
+    databaseURL: "https://trainscheduler-edaa6.firebaseio.com",
+    projectId: "trainscheduler-edaa6",
+    storageBucket: "trainscheduler-edaa6.appspot.com",
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+$("#")
